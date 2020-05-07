42d619fe616da2003c814f5dd2aabd690b758cef
Hugo Cruz <hmartins19@gmail.com>
2020-05-07 20:03:29 -0300

    setup database modificacao

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,11 @@ type User {
   name: String!
   email: String!
 }
-type Query {
-  hello(name: String): String!
-  user(id: ID!): User!
-}
 type Mutation {
   addUser(name: String!, email: String!): User
 }
 `
 const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
-    // this is the user resolver
-    user: (_, { id }) => {
-      return getRepository(User).findOne(id)
-    },
-  },
   Mutation: {
     // this is the addUser resolver
     addUser: (_, { name, email }) => {
