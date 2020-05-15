7ac270fb81316bfc70758f6850d0491fc96013f0
Hugo Cruz <hmartins19@gmail.com>
2020-05-15 19:29:27 -0300

    correcao resolver users

--- src/resolvers/users-query.ts
+++ src/resolvers/users-query.ts
@@ -8,7 +8,7 @@ export default {
 
     Query: {
         
-        async user (_, { users = 20 }, { request }){
+        async users (_, { users = 20 }, { request }){
             
             const token = request.headers.authorization
             
