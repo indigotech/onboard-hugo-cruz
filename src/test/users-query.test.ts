b45a389237226bb9cda77575a20d15bd3f0de34a
Hugo Cruz <hmartins19@gmail.com>
2020-05-19 11:01:25 -0300

    modificacoes sugeridas

--- src/test/users-query.test.ts
+++ src/test/users-query.test.ts
@@ -8,10 +8,13 @@ import * as jwt from 'jsonwebtoken';
 import {AUTHEN_ERROR} from '../errors';
 import {TEN_MINUTES, APP_SECRET} from '../consts';
 
+const NUMBER_OF_USERS = 55;
+
 describe('Users query tests', () => {
 
   let agent;
   let repository: Repository<User>;
+  let token;
 
   const usersQuery = (token, variable?: { limit?, offset?}) => {
 
@@ -42,7 +45,7 @@ describe('Users query tests', () => {
     
     repository = getRepository(User)
 
-    const newusers = Array.from({ length: 55 }).map(() => {
+    const newusers = Array.from({ length: NUMBER_OF_USERS }).map(() => {
         const newUser: User = new User();
         newUser.name = faker.name.findName();
         newUser.email = faker.internet.email();
@@ -56,88 +59,92 @@ describe('Users query tests', () => {
 
   })
 
+  beforeEach( async () => {
+    token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: TEN_MINUTES }); 
+  })
+
   after( async () => {
     await repository.clear()
   })
 
   it('should return the first ten users', async function() {
-      var token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: TEN_MINUTES }); 
+
       const res = await usersQuery(token, { limit: 10, offset: 0 });
 
-      expect(res.body.data.users.total).to.be.equals(55);
+      expect(res.body.data.users.total).to.be.equals(NUMBER_OF_USERS);
       expect(res.body.data.users.info.before).to.be.equals(false);
       expect(res.body.data.users.info.after).to.be.equals(true);
       expect(res.body.data.users.users.length).to.be.equals(10);  
   });
 
   it('should return the last five users (total < offset + limit)', async function() {
-    var token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: TEN_MINUTES }); 
+
     const res = await usersQuery(token, { limit: 10, offset: 50 });
 
-    expect(res.body.data.users.total).to.be.equals(55);
+    expect(res.body.data.users.total).to.be.equals(NUMBER_OF_USERS);
     expect(res.body.data.users.info.before).to.be.equals(true);
     expect(res.body.data.users.info.after).to.be.equals(false);
     expect(res.body.data.users.users.length).to.be.equals(5);   
   });
 
   it('should return all users (limit > total)', async function() {
-    var token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: TEN_MINUTES }); 
+
     const res = await usersQuery(token, { limit: 100, offset: 0 });
 
-    expect(res.body.data.users.total).to.be.equals(55);
+    expect(res.body.data.users.total).to.be.equals(NUMBER_OF_USERS);
     expect(res.body.data.users.info.before).to.be.equals(false);
     expect(res.body.data.users.info.after).to.be.equals(false);
     expect(res.body.data.users.users.length).to.be.equals(55);    
   });
 
   it('should return zero users (offset > total)', async function() {
-    var token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: TEN_MINUTES }); 
+
     const res = await usersQuery(token, { limit: 10, offset: 60 });
 
-    expect(res.body.data.users.total).to.be.equals(55);
+    expect(res.body.data.users.total).to.be.equals(NUMBER_OF_USERS);
     expect(res.body.data.users.info.before).to.be.equals(true);
     expect(res.body.data.users.info.after).to.be.equals(false);
     expect(res.body.data.users.users.length).to.be.equals(0);    
   });
 
   it('should return the first ten users (limit == default, offset == default)', async function() {
-    var token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: TEN_MINUTES }); 
+
     const res = await usersQuery(token);
 
-    expect(res.body.data.users.total).to.be.equals(55);
+    expect(res.body.data.users.total).to.be.equals(NUMBER_OF_USERS);
     expect(res.body.data.users.info.before).to.be.equals(false);
     expect(res.body.data.users.info.after).to.be.equals(true);
     expect(res.body.data.users.users.length).to.be.equals(10);    
   });
 
   it('should return ten users skipping ten (limit == default, offset != default)', async function() {
-    var token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: TEN_MINUTES }); 
+
     const res = await usersQuery(token, { offset: 10 });
 
-    expect(res.body.data.users.total).to.be.equals(55);
+    expect(res.body.data.users.total).to.be.equals(NUMBER_OF_USERS);
     expect(res.body.data.users.info.before).to.be.equals(true);
     expect(res.body.data.users.info.after).to.be.equals(true);
     expect(res.body.data.users.users.length).to.be.equals(10);    
   });
 
   it('should return the first five users (limit != default, offset == default)', async function() {
-    var token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: TEN_MINUTES }); 
+
     const res = await usersQuery(token, { limit: 5 });
 
-    expect(res.body.data.users.total).to.be.equals(55);
+    expect(res.body.data.users.total).to.be.equals(NUMBER_OF_USERS);
     expect(res.body.data.users.info.before).to.be.equals(false);
     expect(res.body.data.users.info.after).to.be.equals(true);
     expect(res.body.data.users.users.length).to.be.equals(5);    
   });
 
   it('should give an error if token is expired', async function() {
-    var token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: 0 }); 
+    token = jwt.sign({ userId: 1 }, APP_SECRET, { expiresIn: 0 }); 
     const res = await usersQuery(token);
     expect(res.body.errors[0].message).to.be.equals(AUTHEN_ERROR);
   })
 
   it('should give an error if token is invalid', async function() {
-    var token = "wrongtoken"
+    token = "wrongtoken"
     const res = await usersQuery(token);
     expect(res.body.errors[0].message).to.be.equals(AUTHEN_ERROR);
   })
